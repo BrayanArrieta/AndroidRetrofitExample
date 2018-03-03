e1254c7f02e10217d6fff88c6edd212c68a0da94
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2018-03-03 14:35:32 -0600

    Add context menu

--- app/src/main/java/com/example/arrieta/androidretrofitexample/MainActivity.java
+++ app/src/main/java/com/example/arrieta/androidretrofitexample/MainActivity.java
@@ -7,7 +7,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -165,4 +167,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.context_menu, menu);
+    }
 }
